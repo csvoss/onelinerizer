fbc77d4ee55e1796f6415e94cd680a2d6a0452a0
Anders Kaseorg <andersk@mit.edu>
2015-09-10 13:12:24 -0400

    Support old-style classes and metaclasses
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- main.py
+++ main.py
@@ -47,7 +47,8 @@ def get_init_code(tree, table):
         __y="(lambda f: (lambda x: x(x))(lambda y:"
           " f(lambda: y(y)())))",
         __g=T("globals()"),
-        __sys="__import__('sys')")
+        __sys="__import__('sys')",
+        __types="__import__('types')")
 
     return output.close()
 
@@ -290,7 +291,12 @@ class Namespace(ast.NodeVisitor):
         body = ns.many_to_one(tree.body, after=T('{__l}'))
         doc = ast.get_docstring(tree, clean=False)
         body = provide(body, __l=repr({} if doc is None else {'__doc__': doc}))
-        class_code = T('type({!r}, ({}), {})').format(tree.name, bases, body)
+        if tree.bases:
+            class_code = T("(lambda __bases, __dict: __dict.get('__metaclass__', getattr(__bases[0], '__class__', {__types}.ClassType))({!r}, __bases, __dict))(({}), {})").format(
+                tree.name, bases, body)
+        else:
+            class_code = T("(lambda __dict: __dict.get('__metaclass__', {__types}.ClassType)({!r}, (), __dict))({})").format(
+                tree.name, body)
         class_code = decoration.format(class_code)
         return assignment_component(T('{after}'), self.store_var(tree.name), class_code)
 
