f34fd2a8dc1dae8e79bc8ae0c3dff5d8ca4f2626
Anders Kaseorg <andersk@mit.edu>
2016-02-25 18:25:26 -0500

    Add missing definitions to unimplemented print_eval_order test
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- tests/unimplemented/print_eval_order.py
+++ tests/unimplemented/print_eval_order.py
@@ -1,3 +1,9 @@
+import sys
+
+def trace(msg, value):
+    print msg
+    return value
+
 print trace('print arg 0', 0), trace('print arg 1', 1),
 print trace('print arg 2', 2), trace('print arg 3', 3)
 print >>trace('print file 0', sys.stdout), trace('print arg 4', 4), trace('print arg 5', 5),
